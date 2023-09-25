fd01a5a62bc76df4ca60237a77a0390ece74576a
Joseph <josephprantar@gmail.com>
2023-09-25 10:10:39 +0800

    Sept.25,2023

---
+++ includes/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        employeeName: 'JavaScript Tutorial',
+        employeeNumber: '85743',
+        paymentStatus: 'Filed',
+        status: 'Pending'
+    },
+    {
+        employeeName: 'CSS Full Course',
+        employeeNumber: '97245',
+        paymentStatus: 'Filed',
+        status: 'Declined'
+    },
+    {
+        employeeName: 'Flex-Box Tutorial',
+        employeeNumber: '36452',
+        paymentStatus: 'Filed',
+        status: 'Approved'
+    },
+]
